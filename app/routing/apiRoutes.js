611ac24c5cf676551e4cc1e07ed8224363cde0a9
Jon C <jchan178@gmail.com>
2018-02-08 22:14:46 -0600

    working

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -9,31 +9,13 @@ module.exports = function(app) {
   });
 
   app.post("/api/friends", function(req, res) {
-  // req.body hosts is equal to the JSON post sent from the user
-  // This works because of our body-parser middleware
-  // var inputData = req.body;
-  // var inputScores = inputData.scores;
-  // var totalDifference = 0;
-  // //var scoresArray = [];
-  // // Loop through each friend's scores  
-  // for (var i = 0; i < inputScores.length; i++) {
-  //  	if(inputScores[i] == "1 (Strongly Disagree)") {
-		// 		inputScores[i] = 1;
-		// 	} else if(inputScores[i] == "5 (Strongly Agree)") {
-		// 		inputScores[i] = 5;
-		// 	} else {
-		// 		inputScores[i] = parseInt(inputScores[i]);
-		// 	}
-		// 	console.log(inputScores);
-   // Add new friend entry
-
-	// Capture the user input object
-	var newFriendScores = req.body.scores;
-	var newFriendScores = [];
+  	// Capture the user input object
+	var newFriendScores = req.body['scores[]'];
+	//var newFriendScores = [];
     var scoresArray = [];
     var friendCount = 0;
     var bestMatch = 0;
-
+    console.log(newFriendScores);
     //runs through all current friends in list
     for (var i = 0; i < friends.length; i++) {
       var totalDifference = 0;
